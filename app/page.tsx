476bf8c63c9a2f69054c35aa3c4525dca55d5ebb
Arthur <arthurferreia1417@gmail.com>
2025-03-18 21:45:37 -0300

    STYLE: continuing the styling of the home section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { LogoPurple } from "../public/images";
+import { LogoPurple, MenuIcon } from "../public/images";
 export default function Home() {
   return (
     <section className="bg-ui-colors-background min-h-screen  text-white w-full h-full">
@@ -8,30 +8,36 @@ export default function Home() {
           src={LogoPurple}
           width={51}
           height={51}
-          alt="Logo da Empresa Data Mastry"
+          alt="Logo da Empresa Data Mastery"
         />
-        <div>menu</div>
-      </header>
-      <div className=" relative bg-transparent ">
-        <div className="bg-primary-indigo z-[3]  blur-2xl absolute opacity-20 -top-20 left-[50%] p-0  w-[15vw] h-[15vw]  aspect-square rounded-full" />
-        <div className="bg-primary-indigo z-[3]  blur-2xl absolute opacity-20 top-48 right-[80%] p-0  w-[25vw] h-[25vw]  aspect-square rounded-full" />
-        <div className="bg-primary-indigo z-[3]  blur-2xl absolute opacity-20 -bottom-20 left-[70%] p-0  w-[25vw] h-[25vw]  aspect-square rounded-full" />
-        {/* <div className=" w-full  absolute z-[2] bg-amber-300 bg-clip-content  rounded-full h-[90dvh] overflow-hidden"></div> */}
-        <div className="w-full h-full relative ">
-          <div
-            className="absolute inset-0 bg-transparent"
-            style={{
-              background:
-                "radial-gradient(circle, rgba(0,0,0,0) 50%, rgba(77,0,133,0.8) 100%)",
-            }}
+        <div>
+          <Image
+            src={MenuIcon}
+            width={30}
+            height={24}
+            alt="Icone do menu lateral"
           />
-          <div className="grid grid-cols-4  relative z-[1] sm:grid-cols-6 lg:grid-cols-[repeat(13,minmax(0,1fr))] lg:grid-rows-9 bg-transparent">
-            {Array.from({ length: 96 }).map((_, i) => (
+        </div>
+      </header>
+      <div className=" relative bg-transparent h-[90dvh]">
+        <div className="bg-primary-indigo z-[15]  blur-2xl absolute opacity-20 -top-20 left-[50%] p-0  w-[15vw] h-[15vw]  aspect-square rounded-full" />
+        <div className="bg-primary-indigo z-[15]  blur-2xl absolute opacity-20 top-48 right-[80%] p-0  w-[25vw] h-[25vw]  aspect-square rounded-full" />
+        <div className="bg-primary-indigo z-[15]  blur-2xl absolute opacity-20 -bottom-20 left-[70%] p-0  w-[25vw] h-[25vw]  aspect-square rounded-full" />
+        <div
+          className="w-full h-full absolute z-10 "
+          style={{
+            background:
+              " radial-gradient(circle, rgba(0,3,25,0) 20%, rgb(0,3,25) 100%)",
+          }}
+        />
+        <div className="w-full h-full relative overflow-hidden rounded-[100%] ">
+          <div className="grid place-items-start grid-cols-4 sm:grid-cols-6 lg:grid-cols-[repeat(13,minmax(0,1fr))] lg:grid-rows-9 bg-transparent">
+            {Array.from({ length: 117 }).map((_, i) => (
               <div
                 key={i}
-                className={`h-full w-full  aspect-square border border-indigo-700/45  ${
-                  [7, 56, 28].includes(i)
-                    ? "bg-indigo-700 opacity-45"
+                className={`h-full w-full  aspect-square border-[0.6px] border-t-0 border-l-0 border-indigo-700/45  ${
+                  [16, 60, 36].includes(i)
+                    ? "bg-indigo-700/45"
                     : "bg-transparent"
                 }`}
               ></div>
