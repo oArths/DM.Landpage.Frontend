19b11d6cec9513fc061d45471294696da4360726
Arthur <arthurferreia1417@gmail.com>
2025-03-20 19:55:17 -0300

    FEAT: implamating a new animation in items

--- app/components/acordeon/index.tsx
+++ app/components/acordeon/index.tsx
@@ -63,38 +63,58 @@ export default function Acordeon() {
           </h2>
           <p className="bg-secondary-purple-heart h-2.5 w-10" />
         </div>
-        <div className={` flex-col  justify-center gap-10 w-auto  items-start pl-20 pr-[120px] ${openMenu === null ? "flex" : "hidden"}`}>
-          <h2 className="font-Inter font-bold text-2xl   ">
-            Nossos clientes & Parceiros
-          </h2>
-          <span className="font-DMSans font-normal text-base text-left">
-            A Data Mastery Tech é uma empresa de serviços educacionais e
-            consultorias especializada em Business Intelligence, Data
-            Engineering, Data Science e Automação Robótica. Nosso propósito é
-            ajudar empresas e profissionais a se desenvolverem na área de dados
-            e gerar transformação digital.
-          </span>
+        <div
+          className={` flex-col  justify-center gap-10 w-auto  items-center pl-20 pr-[120px] ${
+            openMenu === null ? "flex" : "hidden"
+          }`}
+        >
+          <div className="flex flex-col items-start gap-10 w-auto ">
+            <h2 className="font-Inter font-bold text-2xl   ">
+              Nossos clientes & Parceiros
+            </h2>
+            <span className="font-DMSans font-normal text-base text-left">
+              A Data Mastery Tech é uma empresa de serviços educacionais e
+              consultorias especializada em Business Intelligence, Data
+              Engineering, Data Science e Automação Robótica. Nosso propósito é
+              ajudar empresas e profissionais a se desenvolverem na área de
+              dados e gerar transformação digital.
+            </span>
+          </div>
         </div>
         {Data &&
           Data.map((item, index) => (
             <div
-              className="border-purple-scale-stroke border border-t-0 border-r-0 border-b-0 "
+              className="flex flex-row items-center border-purple-scale-stroke border border-t-0 border-r-0 border-b-0"
               key={index}
             >
               <label
-                className="flex flex-row justify-between items-start h-full  px-5 pr-2.5 pl-8 font-Inter font-semibold  text-white/70 text-2xl vertical"
-                onClick={() => setOpenMenu(index)}
+                className="flex flex-row justify-between items-start h-full  px-5 pr-2.5 pl-8 font-Inter font-semibold  text-white/70 text-2xl vertical border-purple-scale-stroke border border-t-0 border-l-0 "
+                onClick={() =>
+                  openMenu === index ? setOpenMenu(null) : setOpenMenu(index)
+                }
               >
                 {item.titulo}
                 <I.ChevronDown
                   color="oklch(76.81% 0.0099 279.64)"
                   size={24}
-                  className="-rotate-90"
+                  className={`${
+                    openMenu === index
+                      ? "animation-rotate-90"
+                      : " animation-reverse-rotate-90"
+                  }`}
                 />
               </label>
-              <div className="hidden flex-col  justify-center gap-10 w-auto  items-start pl-20 pr-[120px]">
-                <h2>{item.titulo}</h2>
-                <span>{item.texto}</span>
+              <div
+                className={`flex-col  justify-center gap-10 w-auto  items-start pl-20 pr-[120px] ${
+                  openMenu === index ? "flex" : "hidden"
+                }`}
+              >
+                <h2 className="font-Inter font-bold text-2xl   ">
+                  {item.titulo}
+                </h2>
+                <span className="font-DMSans font-normal text-base text-left">
+                  {item.texto}
+                </span>
               </div>
             </div>
           ))}
