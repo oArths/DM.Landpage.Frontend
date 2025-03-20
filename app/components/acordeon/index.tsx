2ba6e6dc0d6563292cad08fd318c9ff0a0c75e27
Arthur <arthurferreia1417@gmail.com>
2025-03-20 11:57:28 -0300

    FEAT: adding logic to open menus

--- app/components/acordeon/index.tsx
+++ app/components/acordeon/index.tsx
@@ -1,56 +1,59 @@
+"use client";
 import * as I from "lucide-react";
+import { useState } from "react";
 const Data = [
   {
-    titulo: "Clientes",
+    titulo: "Hermès Paris",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
   {
-    titulo: "Clientes",
+    titulo: "SENAI Sâo Paulo",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
   {
-    titulo: "Clientes",
+    titulo: "Cielo",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
   {
-    titulo: "Clientes",
+    titulo: "Petroplus",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
   {
-    titulo: "Clientes",
+    titulo: "Borkar",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
   {
-    titulo: "Clientes",
+    titulo: "Grenke",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
   {
-    titulo: "Clientes",
+    titulo: "Knauf Ceiling",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
   {
-    titulo: "Clientes",
+    titulo: "Cori",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
   {
-    titulo: "Clientes",
+    titulo: "Locare",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
   {
-    titulo: "Clientes",
+    titulo: "MWM",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
   {
-    titulo: "Clientes",
+    titulo: "Suporte de Domingo",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
   {
-    titulo: "Clientes",
+    titulo: "Ascending",
     texto: "Aqui você encontra todos os clientes cadastrados no sistema.",
   },
 ];
 
 export default function Acordeon() {
+  const [openMenu, setOpenMenu] = useState<number | null>(null);
   return (
     <section className="relative w-full h-[100%] flex items-center ">
       <div className="bg-transparent flex flex-row border-purple-scale-stroke border text-white w-full h-[512px]  ">
@@ -60,7 +63,7 @@ export default function Acordeon() {
           </h2>
           <p className="bg-secondary-purple-heart h-2.5 w-10" />
         </div>
-        <div className="flex flex-col  justify-center gap-10 w-auto  items-start pl-20 pr-[120px]">
+        <div className={` flex-col  justify-center gap-10 w-auto  items-start pl-20 pr-[120px] ${openMenu === null ? "flex" : "hidden"}`}>
           <h2 className="font-Inter font-bold text-2xl   ">
             Nossos clientes & Parceiros
           </h2>
@@ -75,14 +78,21 @@ export default function Acordeon() {
         {Data &&
           Data.map((item, index) => (
             <div
-              className="border-purple-scale-stroke border border-t-0 border-r-0 border-b-0 cursor-pointer"
+              className="border-purple-scale-stroke border border-t-0 border-r-0 border-b-0 "
               key={index}
             >
-              <label className="flex flex-row justify-between items-start h-full  px-5 pr-2.5 pl-8 font-Inter font-semibold text-white/70 text-2xl vertical">
+              <label
+                className="flex flex-row justify-between items-start h-full  px-5 pr-2.5 pl-8 font-Inter font-semibold  text-white/70 text-2xl vertical"
+                onClick={() => setOpenMenu(index)}
+              >
                 {item.titulo}
-                <I.ChevronDown color="oklch(76.81% 0.0099 279.64)" size={24} className="-rotate-90" />
+                <I.ChevronDown
+                  color="oklch(76.81% 0.0099 279.64)"
+                  size={24}
+                  className="-rotate-90"
+                />
               </label>
-              <div className="hidden px-5 pb-5">
+              <div className="hidden flex-col  justify-center gap-10 w-auto  items-start pl-20 pr-[120px]">
                 <h2>{item.titulo}</h2>
                 <span>{item.texto}</span>
               </div>
