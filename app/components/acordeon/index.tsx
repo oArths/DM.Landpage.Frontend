e2230e64e4de87767494a68b7f5b359478211ce0
Arthur <arthurferreia1417@gmail.com>
2025-03-21 16:22:37 -0300

    FEAT: adding a custom animation in arcodion

--- app/components/acordeon/index.tsx
+++ app/components/acordeon/index.tsx
@@ -56,7 +56,7 @@ export default function Acordeon() {
   const [openMenu, setOpenMenu] = useState<number | null>(null);
   return (
     <section className="relative w-full h-[100%] flex items-center ">
-      <div className="bg-transparent flex flex-row border-purple-scale-stroke border text-white w-full h-[512px]  ">
+      <div className="bg-transparent flex flex-row  border-purple-scale-stroke border text-white w-full h-[512px]  ">
         <div className="flex flex-col items-center justify-items-start gap-2.5 px-5 py-3.5 border-purple-scale-stroke border border-l-0 border-b-0 border-t-0">
           <h2 className="font-Inter font-bold text-3xl  h-auto vertical mt-auto">
             Clientes & Parceiros
@@ -64,8 +64,10 @@ export default function Acordeon() {
           <p className="bg-secondary-purple-heart h-2.5 w-10" />
         </div>
         <div
-          className={` flex-col  justify-center gap-10 w-auto  items-center pl-20 pr-[120px] ${
-            openMenu === null ? "flex" : "hidden"
+          className={` flex-col  justify-center gap-10   items-center  ${
+            openMenu === null
+              ? "flex animation-menu"
+              : "animation-reverse-menu  "
           }`}
         >
           <div className="flex flex-col items-start gap-10 w-auto ">
@@ -84,8 +86,10 @@ export default function Acordeon() {
         {Data &&
           Data.map((item, index) => (
             <div
-              className="flex flex-row items-center border-purple-scale-stroke border border-t-0 border-r-0 border-b-0"
               key={index}
+              className={`flex flex-row items-center border-purple-scale-stroke border border-t-0 border-r-0 border-b-0 ${
+                openMenu === index && ""
+              }`}
             >
               <label
                 className="flex flex-row justify-between items-start h-full  px-5 pr-2.5 pl-8 font-Inter font-semibold  text-white/70 text-2xl vertical border-purple-scale-stroke border border-t-0 border-l-0 "
@@ -105,14 +109,16 @@ export default function Acordeon() {
                 />
               </label>
               <div
-                className={`flex-col  justify-center gap-10 w-auto  items-start pl-20 pr-[120px] ${
-                  openMenu === index ? "flex" : "hidden"
+                className={`flex-col  justify-center gap-10  items-start overflow-hidden ${
+                  openMenu === index
+                    ? "flex animation-menu"
+                    : "hidden"
                 }`}
               >
-                <h2 className="font-Inter font-bold text-2xl   ">
+                <h2 className="font-Inter font-bold text-2xl animation-text  ">
                   {item.titulo}
                 </h2>
-                <span className="font-DMSans font-normal text-base text-left">
+                <span className="font-DMSans font-normal text-base text-left animation-text">
                   {item.texto}
                 </span>
               </div>
