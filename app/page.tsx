efa8b2761d4ef265eac8cb5e50627a5959495e15
Arthur <arthurferreia1417@gmail.com>
2025-03-23 12:57:59 -0300

    STYLE: adding a style in section

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Header from "./templates/header";
 import Home from "./templates/home";
 export default function Main() {
   return (
-    <main className="bg-ui-colors-background min-h-screen flex flex-col items-center text-white w-full h-full">
+    <main className="bg-ui-colors-background min-h-screen flex flex-col items-center overflow-hidden text-white w-full h-full">
       <Header />
       <Home />
-      <Acordeon/>
+      <Acordeon />
     </main>
   );
 }
