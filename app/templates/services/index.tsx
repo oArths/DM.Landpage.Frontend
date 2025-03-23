dad1663e9aa733b6b9534d0a7880962766f39ddd
Arthur <arthurferreia1417@gmail.com>
2025-03-23 12:58:16 -0300

    FEAT: create a new section

---
+++ app/templates/services/index.tsx
@@ -0,0 +1,9 @@
+export default function Service() {
+  return (
+    <div className="bg-ui-colors-background min-h-screen flex flex-col items-center overflow-hidden text-white w-full h-full">
+
+
+        
+    </div>
+  );
+}
