b927cbd4af6067aaf84575adba5f637609c242f9
OArths <arthurferreira1417@gmail.com>
2026-07-23 01:01:51 -0300

    Feat: add services section and dividers to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Acordeon from "./components/acordeon";
 import Header from "./templates/header";
 import Home from "./templates/home";
+import Service from "./templates/services";
 export default function Main() {
   return (
     <main className="bg-ui-colors-background min-h-screen flex flex-col items-center overflow-hidden text-white w-full h-full">
       <Header />
       <Home />
       <Acordeon />
+      <div className="bg-grey-scale-onyx w-[60%] h-[1px] my-40" />
+      <Service />
+      <div className="bg-grey-scale-onyx w-[60%] h-[1px] my-40" />
     </main>
   );
 }
