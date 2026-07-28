53fbd63afa8483be1431b716f00a14f681f38123
OArths <arthurferreira1417@gmail.com>
2026-07-27 23:21:05 -0300

    feat: add section IDs for anchor navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-import Acordeon from "./components/acordeon";
-import Header from "./templates/header";
 import Home from "./templates/home";
+import Contact from "./templates/contact";
 import Service from "./templates/services";
+import Acordeon from "./components/acordeon";
 export default function Main() {
   return (
-    <main className="bg-ui-colors-background min-h-screen flex flex-col items-center overflow-hidden text-white w-full h-full">
-      <Header />
-      <Home />
-      <Acordeon />
+    <main className="min-h-screen flex flex-col items-center overflow-hidden text-white w-full h-full scroll-smooth">
+      <Home id="empresa" />
+      <Acordeon id="clientes" />
       <div className="bg-grey-scale-onyx w-[60%] h-[1px] my-40" />
-      <Service />
+      <Service id="servicos" />
+      <div className="bg-grey-scale-onyx w-[60%] h-[1px] my-40" />
+      <Contact id="contato" />
       <div className="bg-grey-scale-onyx w-[60%] h-[1px] my-40" />
     </main>
   );
